6d9d6f5c2176304148f40d437d2d99249f60883e
crsolver <andres24k8@gmail.com>
2026-02-04 10:20:44 -0600

    fix links

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        editor: resolve(__dirname, 'editor.html'),
+      },
+    },
+  },
+})
